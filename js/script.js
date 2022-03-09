0ee29b171a65333140714f12e0aa4091ff8406fe
MattiaPortanti <mattia.portantipm@gmail.com>
2022-03-09 16:04:53 +0100

    impostazione file di lavoro, creazione struttura statica nel HTML e risouzione primo Milestone

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const team = [
+    {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    image: 'wayne-barnett-founder-ceo.jpg',
+},
+{
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg',
+},
+{
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg',
+},
+{
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg',
+},
+{
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg',
+},
+{
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg',
+},
+];
+
+//! Milestone 1
+for (let i = 0; i < team.length; i = i + 1){
+    console.log(team[i].name);
+    console.log(team[i].role);
+    console.log(team[i].image);
+};
+
+//!Milestone 2 
+for (let i = 0; i < team.length; i = i + 1){
+    console.log(team[i].name);
+    // console.log(team[i].role);
+    // console.log(team[i].image);
+};
